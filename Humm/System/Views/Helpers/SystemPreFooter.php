9247d2aa59ac444a4ff197bab50f89544aa325c3
David Esperalta <info@davidesperalta.com>
2014-10-05 09:56:08 +0200

    Remove the Humm PHP link target blank attribute.

--- Humm/System/Views/Helpers/SystemPreFooter.php
+++ Humm/System/Views/Helpers/SystemPreFooter.php
@@ -9,9 +9,8 @@
     </div>
     <div id="copyrightNote">
      ©2014 Humm PHP - <a href="<?= $hummPhpSiteUrl ?>"
-      target="_blank" title="<?php e('Visit the Humm PHP website') ?>">www.hummphp.com</a>
+      title="<?php e('Visit the Humm PHP website') ?>">www.hummphp.com</a>
     </div>
    </footer>
   </div>
   <!-- /contentsWrapper -->
-  

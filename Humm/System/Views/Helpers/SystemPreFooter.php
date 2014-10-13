34f1bb8c75fd0c396a5ca24192f72f33242c112c
David Esperalta <info@davidesperalta.com>
2014-10-13 18:36:25 +0200

    Change HTML small tag for a more appropiate span tag.

--- Humm/System/Views/Helpers/SystemPreFooter.php
+++ Humm/System/Views/Helpers/SystemPreFooter.php
@@ -5,7 +5,7 @@
     <div id="poweredNote">
      <a href="<?= $hummPhpSiteUrl ?>" target="_blank"
       title="<?php e('Visit the Humm PHP website') ?>"><?php e('Powered by Humm PHP') ?>
-       <small id="hummVersion"><?= $hummVersion ?></small></a>
+       <span class="hummVersion"><?= $hummVersion ?></span></a>
     </div>
     <div id="copyrightNote">
      ©2014 Humm PHP - <a href="<?= $hummPhpSiteUrl ?>"

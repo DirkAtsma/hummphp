e3155c07ac51cfb85c5c09fd442c66d17fae9168
David Esperalta <info@davidesperalta.com>
2016-10-11 20:37:23 +0200

    Prepare a www folder
    Move the Humm PHP properly files into the "www" folder.

---
+++ www/Humm/Sites/Main/Views/Helpers/PreHeader.php
@@ -0,0 +1,10 @@
+
+<?php if (!$this) { exit; } ?>
+
+<!DOCTYPE html>
+<html lang="<?= $siteLanguage ?>" dir="<?= $siteLanguageDir ?>">
+ <head>
+  <meta charset="utf-8">
+  <meta name="generator" content="Humm PHP: http://www.davidesperalta.com/" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <link rel="stylesheet" type="text/css" href="<?= $viewsStylesUrl ?>Main.css" media="all" />
---
+++ www/Humm/System/Version/Version.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * This file define some Humm PHP version related constants.
+ *
+ * To have an easy access place to update version information.
+ *
+ * @author D. Esperalta <info@davidesperalta.com>
+ * @link http://www.davidesperalta.com/
+ * @license https://www.gnu.org/licenses/gpl.html
+ * @copyright (C)2016 Humm PHP - David Esperalta
+ */
+
+/**
+ * Define the version of Humm PHP.
+ */
+\define('HUMM_VERSION_STRING', '2016.5 (09/07/2016)');
+
+/**
+ * Define the release date of Humm PHP.
+ */
+\define('HUMM_VERSION_RELEASE', '09/07/2016');
